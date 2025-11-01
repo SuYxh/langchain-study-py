05be6db0d7e86f2f7e82410f3d7efcd137625f70
“杨鑫昊” <yangxinhao.9527@bytedance.com>
2025-11-01 17:58:44 +0800

    feat(graph): 添加两个流式图处理示例脚本

---
+++ src/graph/14.graph-Edge-Stream-1.py
@@ -0,0 +1,33 @@
+# Subgraph与graph使用相同State
+from operator import add
+from typing import TypedDict, Annotated
+
+from langgraph.constants import END
+from langgraph.graph import StateGraph, MessagesState, START
+
+class State(TypedDict):
+    messages: Annotated[list[str],add]
+
+# Subgraph
+def sub_node_1(state:State) -> MessagesState:
+    return {"messages":["response from subgraph"]}
+
+subgraph_builder = StateGraph(State)
+subgraph_builder.add_node("sub_node_1", sub_node_1)
+subgraph_builder.add_edge(START, "sub_node_1")
+subgraph_builder.add_edge("sub_node_1", END)
+
+subgraph = subgraph_builder.compile()
+
+# Parent graph
+builder = StateGraph(State)
+builder.add_node("subgraph_node", subgraph)
+
+builder.add_edge(START, "subgraph_node")
+builder.add_edge("subgraph_node", END)
+
+graph = builder.compile()
+
+# for chunk in graph.stream({"messages": ["hello subgraph"]},stream_mode="updates"):
+for chunk in graph.stream({"messages": ["hello subgraph"]},stream_mode="debug"):
+    print(chunk)
---
+++ src/graph/15.graph-Edge-Stream-2.py
@@ -0,0 +1,33 @@
+from typing import TypedDict
+from langgraph.config import get_stream_writer
+from langgraph.graph import StateGraph, START, END
+
+class State(TypedDict):
+    query: str
+    answer: str
+
+def node(state: State):
+    writer = get_stream_writer()
+    writer({"自定义key": "在节点内返回自定义信息"})
+    return {"answer": "some data"}
+
+def node2(state: State):
+    writer = get_stream_writer()
+    writer({"自定义key": "在节点内返回自定义信息2"})
+    return {"answer": "some data"}
+
+graph = (
+    StateGraph(State)
+    .add_node(node)
+    .add_node(node2)
+    .add_edge(START, "node")
+    .add_edge("node", "node2")
+    .add_edge("node2", END)
+    .compile()
+)
+
+inputs = {"query": "example"}
+
+# Usage
+for chunk in graph.stream(inputs, stream_mode="custom"):
+    print(chunk)
