89db4a6f54c6ef4cc44302bc0e36214a1176caca
“杨鑫昊” <yangxinhao.9527@bytedance.com>
2025-11-01 17:26:50 +0800

    docs(graph): 添加关于条件路由的注释说明

--- src/graph/10.graph-Edge-条件边.py
+++ src/graph/10.graph-Edge-条件边.py
@@ -24,9 +24,21 @@ def routing_func(state: State) -> str:
     else:
         return END
 
+# 如果不想在路由函数中写⼊过多具体的节点名称，也可以在函数中返回⼀个⾃定义的结果，然后将这个结果解析到某⼀个具体的Node上。例如
+# def routing_func(state: State) -> bool:
+#     if state["number"] > 5:
+#         return True
+#     else:
+#         return False
+
+# builder.add_conditional_edges(START, routing_func, {True: "node_a", False: "node_b"})
+
 
 builder.add_conditional_edges(START, routing_func)
 builder.add_edge("node1", END)
 
 graph = builder.compile()
 print(graph.invoke({"number": 7}))
+
+
+
